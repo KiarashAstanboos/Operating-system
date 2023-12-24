dbcf3c6adeaf699d3156c4ab6e97d920721898db
KiarashAstanboos <Kiarash.astanboos@gmail.com>
2023-12-24 18:43:24 +0330

    "recursive loop that will return the path of all files in a directory and it's subdirectories"

--- os.c
+++ os.c
@@ -1,7 +1,37 @@
+#define _GNU_SOURCE
 #include <stdio.h>
-void main(){
-    printf("hello");
-    printf("\n2");
-    printf("\n2");
-    printf("\n2\n");
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+
+void printFiles(char *path) {
+    DIR *dir = opendir(path);
+    struct dirent *entry;
+
+    if (dir == NULL) {
+        return;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+                continue;
+            }
+            char pathName[1024];
+            snprintf(pathName, sizeof(pathName), "%s/%s", path, entry->d_name);
+            printFiles(pathName);
+        } else {
+            printf("%s/%s\n", path, entry->d_name);
+        }
+    }
+
+    closedir(dir);
+}
+
+int main() {
+    char path[1024];
+    // printf("Enter the path to the directory: ");
+    // scanf("%s", path);
+    printFiles("/home/kiarash/test");
+    return 0;
 }
